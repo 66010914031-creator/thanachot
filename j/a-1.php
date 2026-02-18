cbf7308b06d99476ca6a68f8c496744665dd0ab2
thanachot <66010914031@msu.ac.th>
2026-02-18 17:01:56 +0700

    j

---
+++ j/a-1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ธนโชติ วงศ์ดี (กาย)</title>
+</head>
+
+<body>
+<h1>66010914031ธนโชติ วงศ์ดี (กาย)</h1>
+<img src = "1.png" height="350">
+
+
+</body>
+</html>
